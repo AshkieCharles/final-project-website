1973f5fcaac063c77f8e5d10477a4e35087ae94b
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-10-02 08:51:03 -0600

    minor changes

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a').forEach(link => {
   if(link.href.includes(`${activePage}`)){
     link.classList.add('active');
-    console.log(link);
   }
 })
 
